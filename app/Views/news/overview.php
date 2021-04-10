9ec722707871e830ccdda2f8dd6d1fb10b858c4f
tgarciafo <tgarciafo@uoc.edu>
2021-04-10 17:14:35 +0200

    afegir enllaç per tornar a la llista de notícies

--- app/Views/news/overview.php
+++ app/Views/news/overview.php
@@ -14,6 +14,7 @@
         
 
     <?php endforeach; ?>
+
     <?= $pager->links() ?>
 
 
--- app/Views/news/view.php
+++ app/Views/news/view.php
@@ -3,3 +3,5 @@
 <p>Data de publicació: <?= esc(date("d-m-Y", strtotime($news['publication_date']))) ?></p>
 <p>Categoria: <?= esc($news['category']) ?></p>
 <p><?= esc($news['content']) ?></p>
+
+<a href="<?= base_url('https://localhost:8080') ?>">Tornar a la llista de notícies</a>
