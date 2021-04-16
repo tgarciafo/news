e41f7f2e867375403e81b5706a51462fdeb67850
tgarciafo <tgarciafo@uoc.edu>
2021-04-16 20:30:13 +0200

    canvi nom images

--- app/Views/news/view.php
+++ app/Views/news/view.php
@@ -1,5 +1,5 @@
 <h2><?= esc($news['author']) ?></h2>
-<img src="<?php echo base_url('images/' . esc($news['image'])); ?>" alt=""/>
+<img src="<?php echo base_url('Images/' . esc($news['image'])); ?>" alt=""/>
 <p>Data de publicació: <?= esc(date("d-m-Y", strtotime($news['publication_date']))) ?></p>
 <p>Categoria: <?= esc($news['category']) ?></p>
 <p><?= esc($news['content']) ?></p>
